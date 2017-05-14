49c930919946bb00cbbb15ae1d30661ad16911b1
Viktor Sobol <sddd1995@mail.ru>
2017-05-14 12:29:16 +0200

    gorotines

--- Samples/main.go
+++ Samples/main.go
@@ -1,1 +1,21 @@
-package Samples
+package main
+
+import "fmt"
+import "time"
+import "math/rand"
+
+func printString(from string) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+		time.Sleep((time.Second * time.Duration(r.Int31n(10))))
+	}
+}
+
+func main() {
+	go printString ("Viktor")
+	printString("Vitya")
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("done")
+}
