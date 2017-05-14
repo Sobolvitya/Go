bd1461e28fd5f56c3b6e60618272c9740166ee43
Viktor Sobol <sddd1995@mail.ru>
2017-05-14 15:15:11 +0200

    Start lab4

--- Samples/main.go
+++ Samples/main.go
@@ -13,9 +13,44 @@ func printString(from string) {
 }
 
 func main() {
-	go printString ("Viktor")
-	printString("Vitya")
-	var input string
-	fmt.Scanln(&input)
-	fmt.Println("done")
+	
+	ticker := time.NewTicker(500 * time.Millisecond)
+	
+	go func() {
+		for t := range ticker.C {
+			fmt.Println("Tick at", t)
+		}
+	} ()
+	time.Sleep(1550 * time.Millisecond)
+	ticker.Stop()
+
+	//c1 := make(chan string, 2)
+	//
+	//go func() {
+	//	time.Sleep(1 * time.Second)
+	//	c1 <- "c1"
+	//}()
+	//go func() {
+	//	time.Sleep(1 * time.Second)
+	//	c1 <- "c2"
+	//}()
+	//
+	//msg1 := <-c1;
+	//msg2 := <-c1;
+	//
+	//fmt.Println(msg1)
+	//fmt.Println(msg2)
+	
+	
+	//for i := 0; i < 2; i++ {
+	//	select {
+	//	case msg:= <-c1:
+	//		fmt.Println(msg)
+	//	case msg:= <-c2:
+	//		fmt.Println(msg)
+	//	}
+	//}
+	//
+	//fmt.Println(<-messages)
+	//fmt.Println(<-messages)
 }
